bbd26ffee23f2086798aeb77e741b32ae02a6413
DanielCC02 <danielcoto2004@gmail.com>
2026-03-24 02:00:44 -0600

    feat: add create user function

---
+++ FinanceTrackerSystem/FinanceTracker.Application/Features/Transactions/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace FinanceTracker.Application.Features.Transactions.Commands.CreateUser
+{
+    public record CreateUserCommand
+    (
+        string Name,
+        string Email,
+        string Password
+    ) : IRequest<Guid>;
+}
