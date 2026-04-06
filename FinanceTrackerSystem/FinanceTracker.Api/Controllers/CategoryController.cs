5189caa334aa9cc8dfe87c9fbe90d9420bc66309
DanielCC02 <danielcoto2004@gmail.com>
2026-04-05 23:58:14 -0600

    Controllers already done

--- FinanceTrackerSystem/FinanceTracker.Api/Controllers/CategoryController.cs
+++ FinanceTrackerSystem/FinanceTracker.Api/Controllers/CategoryController.cs
@@ -1,11 +1,18 @@
 ﻿using FinanceTracker.Application.Features.Categories.Commands.CreateCategory;
+using FinanceTracker.Application.Features.Categories.Commands.DeleteCategory;
+using FinanceTracker.Application.Features.Categories.Commands.UpdateCategory;
+using FinanceTracker.Application.Features.Categories.DTOs;
+using FinanceTracker.Application.Features.Categories.Queries.GetCategories;
+using FinanceTracker.Application.Features.Categories.Queries.GetCategoryById;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinanceTracker.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class CategoryController : ControllerBase
     {
         private readonly IMediator _mediator;
@@ -18,8 +25,40 @@ namespace FinanceTracker.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreateCategoryCommand command)
         {
-            var id = await _mediator.Send(command);
-            return Ok(id);
+            var result = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<CategoryDto>>> GetAll()
+        {
+            var categories = await _mediator.Send(new GetCategoriesQuery());
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryDto>> GetById(Guid id)
+        {
+            var category = await _mediator.Send(new GetCategoryByIdQuery(id));
+            return Ok(category);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<CategoryDto>> Update(Guid id, UpdateCategoryCommand command)
+        {
+            if (id != command.Id)
+                return BadRequest("ID mismatch");
+
+            var updated = await _mediator.Send(command);
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")] // 🔥 solo admin puede borrar
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _mediator.Send(new DeleteCategoryCommand(id));
+            return NoContent();
         }
     }
 }
--- FinanceTrackerSystem/FinanceTracker.Api/Controllers/TransactionsController.cs
+++ FinanceTrackerSystem/FinanceTracker.Api/Controllers/TransactionsController.cs
@@ -1,11 +1,18 @@
 ﻿using FinanceTracker.Application.Features.Transactions.Commands.CreateTransaction;
+using FinanceTracker.Application.Features.Transactions.Commands.DeleteTransaction;
+using FinanceTracker.Application.Features.Transactions.Commands.UpdateTransaction;
+using FinanceTracker.Application.Features.Transactions.DTOs;
+using FinanceTracker.Application.Features.Transactions.Queries.GetTransactionById;
+using FinanceTracker.Application.Features.Transactions.Queries.GetTransactions;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinanceTracker.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class TransactionsController : ControllerBase
     {
         private readonly IMediator _mediator;
@@ -18,8 +25,40 @@ namespace FinanceTracker.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreateTransactionCommand command)
         {
-            var id = await _mediator.Send(command);
-            return Ok(id);
+            var result = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TransactionDto>>> GetAll()
+        {
+            var transactions = await _mediator.Send(new GetTransactionsQuery());
+            return Ok(transactions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TransactionDto>> GetById(Guid id)
+        {
+            var transaction = await _mediator.Send(new GetTransactionByIdQuery(id));
+            return Ok(transaction);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<TransactionDto>> Update(Guid id, UpdateTransactionCommand command)
+        {
+            if (id != command.Id)
+                return BadRequest("ID mismatch");
+
+            var updated = await _mediator.Send(command);
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")] // 🔥 solo admin
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _mediator.Send(new DeleteTransactionCommand(id));
+            return NoContent();
         }
 
     }
